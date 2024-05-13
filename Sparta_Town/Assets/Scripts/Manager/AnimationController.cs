ec5ef848bc3ec85aacc5232ef0e3f2e8a660d8db
J-Kaun <kangeun9727@gmail.com>
2024-05-13 13:54:38 +0900

    Add_Animation
    캐릭터 대기 모션 및 이동 모션 애니메이션 추가

---
+++ Sparta_Town/Assets/Scripts/Manager/AnimationController.cs
@@ -0,0 +1,14 @@
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    protected Animator animator;
+    protected TopDownController controller;
+
+    protected virtual void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        controller = GetComponent<TopDownController>();
+    }
+}
